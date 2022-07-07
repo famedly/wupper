9ca1406ed4ff34b71f99ffb2951cd4c1adb22d40
h.carnot <h.carnot@famedly.com>
2022-07-07 14:01:04 +0200

    fix: state propagation for stateful widgets

--- example/lib/stateful_widget_test.dart
+++ example/lib/stateful_widget_test.dart
@@ -1,6 +1,8 @@
 import 'package:wupper/wupper.dart';
 
 class StatefulWidgetTest extends StatefulWidget {
+  const StatefulWidgetTest() : super();
+
   @override
   StateWidget<StatefulWidget> createState() => _StatefulWidgetTestState();
 }
@@ -16,20 +18,31 @@ class _StatefulWidgetTestState extends StateWidget<StatefulWidgetTest> {
 
   @override
   Widget build(BuildContext context) {
-    print("Build stateful widget from the text");
     return DivElementWidget(children: [
       ParagraphElementWidget(text: "Count: $count"),
       ButtonElementWidget(
         text: 'Add',
         onClick: add,
       ),
+
+      ParagraphElementWidget(text: "Counted: $count"),
+      // to demonstrate what happens when constructor is not const
+      // ignore: prefer_const_constructors
+      StateWidgetTest(),
+      TimeWidgetTestNotConst(),
+
+      ParagraphElementWidget(text: "TimeWrapper"),
+      TimeWidgetWrapper(),
+      TimeWidgetWrapper(),
+      TimeWidgetWrapper(),
+
+      ParagraphElementWidget(text: "Some other random tests"),
       const StateWidgetTest(),
       const TimeWidgetTest(),
-      const StateWidgetTest(),
       const TimeWidgetTest(),
       const TimeWidgetTest(),
       const TimeWidgetTest(),
-      const TimeWidgetTest(),
+      ParagraphElementWidget(text: "Count: $count"),
       const TimeWidgetTest(),
       const TimeWidgetTest()
     ]);
@@ -43,19 +56,30 @@ class StateWidgetTest extends StatefulWidget {
 }
 
 class _StateWidgetTestState extends StateWidget<StateWidgetTest> {
+  int buildCount = 0;
+
   @override
   Widget build(BuildContext context) {
-    print("Build state widget test");
+    buildCount++;
+
     final date = DateTime.now().toString();
 
     return DivElementWidget(children: [
+      ParagraphElementWidget(
+          text: "Start of stateful widget. Build count: $buildCount"),
       ParagraphElementWidget(text: "Statefull widget: $date"),
       const TimeWidgetTest(),
       const TimeWidgetTest(),
-      const TimeWidgetTest(),
-      ParagraphElementWidget(text: "end of test")
+      TimeWidgetTestNotConst(),
+      TimeWidgetTestNotConst(),
+      ParagraphElementWidget(text: "end of test widget")
     ]);
   }
+}
+
+class TimeWidgetWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => const TimeWidgetTest();
 }
 
 class TimeWidgetTest extends StatelessWidget {
@@ -63,9 +87,19 @@ class TimeWidgetTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("Build widget test");
     final date = DateTime.now().toString();
 
-    return ParagraphElementWidget(text: "Stateless widget: $date");
+    return ParagraphElementWidget(
+        text: "I'm const (so shouldn't update): $date");
+  }
+}
+
+class TimeWidgetTestNotConst extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final date = DateTime.now().toString();
+
+    return ParagraphElementWidget(
+        text: "I' NOT const (so I SHOULD update): $date");
   }
 }
