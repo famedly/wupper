97c4260733f73721c5468d3acce2b5c1ca44376a
h.carnot <h.carnot@famedly.com>
2022-07-14 16:44:14 +0200

    fix: added checkbox

--- lib/src/html_element_list.dart
+++ lib/src/html_element_list.dart
@@ -835,7 +835,7 @@ class ButtonElementWidget extends HtmlElementWidget {
   Element render(BuildContext context) {
     final v = ButtonElement();
     if (type != null) v.type = type!;
-    if(disabled != null) v.disabled = disabled!;
+    if (disabled != null) v.disabled = disabled!;
     return hook(context, v);
   }
 }
@@ -1444,6 +1444,117 @@ class DivElementWidget extends HtmlElementWidget {
       super.onWheel});
   @override
   Element render(BuildContext context) => hook(context, DivElement());
+}
+
+class CheckboxInputElementWidget extends HtmlElementWidget {
+  final bool? checked;
+
+  CheckboxInputElementWidget(
+      {this.checked,
+      super.nonce,
+      super.attributes,
+      super.children,
+      super.classes,
+      super.dataset,
+      super.innerHtml,
+      super.innerText,
+      super.scrollLeft,
+      super.scrollTop,
+      super.contentEditable,
+      super.dir,
+      super.draggable,
+      super.hidden,
+      super.inert,
+      super.inputMode,
+      super.lang,
+      super.spellcheck,
+      super.tabIndex,
+      super.title,
+      super.translate,
+      super.className,
+      super.id,
+      super.slot,
+      super.nodes,
+      super.text,
+      super.onAbort,
+      super.onBeforeCopy,
+      super.onBeforeCut,
+      super.onBeforePaste,
+      super.onBlur,
+      super.onCanPlay,
+      super.onCanPlayThrough,
+      super.onChange,
+      super.onClick,
+      super.onContextMenu,
+      super.onCopy,
+      super.onCut,
+      super.onDoubleClick,
+      super.onDrag,
+      super.onDragEnd,
+      super.onDragEnter,
+      super.onDragLeave,
+      super.onDragOver,
+      super.onDragStart,
+      super.onDrop,
+      super.onDurationChange,
+      super.onEmptied,
+      super.onEnded,
+      super.onError,
+      super.onFocus,
+      super.onInput,
+      super.onInvalid,
+      super.onKeyDown,
+      super.onKeyPress,
+      super.onKeyUp,
+      super.onLoad,
+      super.onLoadedData,
+      super.onLoadedMetadata,
+      super.onMouseDown,
+      super.onMouseEnter,
+      super.onMouseLeave,
+      super.onMouseMove,
+      super.onMouseOut,
+      super.onMouseOver,
+      super.onMouseUp,
+      super.onMouseWheel,
+      super.onPaste,
+      super.onPause,
+      super.onPlay,
+      super.onPlaying,
+      super.onRateChange,
+      super.onReset,
+      super.onResize,
+      super.onScroll,
+      super.onSearch,
+      super.onSeeked,
+      super.onSeeking,
+      super.onSelect,
+      super.onSelectStart,
+      super.onStalled,
+      super.onSubmit,
+      super.onSuspend,
+      super.onTimeUpdate,
+      super.onTouchCancel,
+      super.onTouchEnd,
+      super.onTouchEnter,
+      super.onTouchLeave,
+      super.onTouchMove,
+      super.onTouchStart,
+      super.onTransitionEnd,
+      super.onVolumeChange,
+      super.onWaiting,
+      super.onFullscreenChange,
+      super.onFullscreenError,
+      super.onWheel});
+
+  @override
+  Element render(BuildContext context) {
+    final v = CheckboxInputElement();
+
+    if (checked != null) v.checked = checked!;
+
+    return hook(context, v);
+  }
 }
 
 class EmbedElementWidget extends HtmlElementWidget {
