1f58240d8f4f11db3892db39cc0ebad737caa7f8
h.carnot <h.carnot@famedly.com>
2022-10-27 18:03:05 +0200

    feat: add on change listener

--- example/lib/test_inputs.dart
+++ example/lib/test_inputs.dart
@@ -8,6 +8,22 @@ class TestInput extends StatefulWidget {
 
 class TestInputState extends StateWidget<TestInput> {
   final controller = InputElementController();
+  final refreshController = InputElementController();
+
+  String? result;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshController.addListener(onChange);
+  }
+
+  void onChange() {
+    setState(() {
+      result = refreshController.value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DivElementWidget(children: [
@@ -21,7 +37,12 @@ class TestInputState extends StateWidget<TestInput> {
           future: Future.delayed(
               const Duration(seconds: 1), () => DateTime.now().toString()),
           builder: (context, snap) =>
-              ParagraphElementWidget(text: "Result ${snap.data}"))
+              ParagraphElementWidget(text: "Result ${snap.data}")),
+      ParagraphElementWidget(
+          text: "Type and see the interface refresh while typing"),
+      InputElementWidget(
+          controller: refreshController, title: DateTime.now().toString()),
+      if (result != null) ParagraphElementWidget(text: "Result: $result")
     ]);
   }
 }
